00dbf20b48333bce550978459ff1e27060eb97ce
Daniel Munch <daniellmunch@gmail.com>
2018-05-26 16:45:11 +0200

    Added initial things

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # discord-bot3
 My Discord Bot 3
+
+Please only use if you've got permission to!
+
+All credits go to me :D - Daniel Munch, A.K.A. Munhcii
---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
+
+bot.run('<NDQ5MjMyNTc4NzIyMTM2MTA1.DesDzg.VaD6sXFvdGduQEmkTaPZUlqWYBk>')
